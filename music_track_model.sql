d4bcbfdf85b3c74867f3259e6b63d9e752f54a3d
neuro <yamsh@yandex.ru>
2023-06-01 12:26:54 +0600

    music_track.db code one-to-many db

---
+++ music_track_model.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Artist(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE, name TEXT);
+CREATE TABLE Album (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE, artist_id INTEGER, title TEXT );
+CREATE TABLE Genre(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE, name TEXT);
+CREATE TABLE Track(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE, album_id INTEGER, genre_id INTEGER, len INTEGER, rating INTEGER, title TEXT, count INTEGER);
+INSERT INTO Artist (name) VALUES ('Led Zepplin');
+INSERT INTO Artist (name) VALUES ('AC/DC');
+INSERT INTO Genre (name) VALUES ('Rock') ;
+INSERT INTO Genre (name) VALUES ('Metal');
+INSERT INTO Album (title, artist_id) VALUES ('Who Made Who', 2);
+INSERT INTO Album (title, artist_id) VALUES ('IV', 1);
+INSERT INTO Track (title, rating, len, count, album_id, genre_id) VALUES ('Black Dog', 5, 297, 0, 2, 1) ;
+INSERT INTO Track (title, rating, len, count, album_id, genre_id) VALUES ('Stairway', 5, 482, 0, 2, 1) ;
+INSERT INTO Track (title, rating, len, count, album_id, genre_id) VALUES ('About to Rock', 5, 313, 0, 1, 2) ;
+INSERT INTO Track (title, rating, len, count, album_id, genre_id) VALUES ('Who Made Who', 5, 207, 0, 1, 2) ; 
